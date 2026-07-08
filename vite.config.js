af656d530d52a3c9ed4acf12a15071f37315efb9
mage__ <prod.magegr@gmail.com>
2026-07-09 00:16:28 +0700

    Fix Vite base path and update title

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/GenAI/',
+  base: process.env.NODE_ENV === 'production' ? '/GenAI/' : '/',
 })
